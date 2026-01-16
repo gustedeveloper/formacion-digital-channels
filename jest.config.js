ab53f023ad473ea29a49b62ed06aece14e86a3e4
gustedeveloper <219.guste@gmail.com>
2026-01-16 09:03:48 +0100

    install and setup jest for testing

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+export default {
+  preset: 'jest-preset-angular',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': 'ts-jest',
+  },
+};
